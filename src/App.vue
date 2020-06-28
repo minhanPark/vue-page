b5b74c216e2808e6870f64d35dfb8a4d2cbbcbdb
minhan <alsgks1218@gmail.com>
2020-06-29 00:36:22 +0900

    add finding search list func

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Search v-on:searchList="searchList"></Search>
+  </div>
 </template>
 
 <script>
-export default {};
+import Search from "./components/search";
+
+export default {
+  data() {
+    return {
+      stores: [
+        { id: 1, name: "옵타움플랫폼", business: "법인", tel: "0517777777" },
+        { id: 2, name: "강가네국밥", business: "개인", tel: "0511234567" },
+        { id: 3, name: "법인회사예시", business: "법인", tel: "0513456789" }
+      ],
+      storeList: [],
+      nextId: 4, // 추가되었을 때 적용되어야할 id를 나타냄
+      inputValue: "",
+      selectedStore: {}
+    };
+  },
+  components: {
+    Search: Search
+  },
+  methods: {
+    searchList(condition, value) {
+      this.storeList = this.stores.filter(store => store[condition] === value);
+      console.log(this.storeList);
+    }
+  }
+};
 </script>
 
 <style>
---
+++ src/components/search.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <form v-on:submit.prevent>
+      <select v-model="selectedCondition">
+        <option value="선택 없음">선택 없음</option>
+        <option value="name">상점명</option>
+        <option value="business">사업자</option>
+      </select>
+      <input v-model="searchValue" v-on:keyup.enter="searchList" />
+      <ul>
+        <li>
+          <i class="fa fa-search"></i>
+          <span>조회</span>
+        </li>
+        <li>
+          <i class="fa fa-plus"></i>
+          <span>신규가맹점</span>
+        </li>
+      </ul>
+    </form>
+    <span>{{selectedCondition}}</span>
+    <span>{{searchValue}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedCondition: "선택 없음",
+      searchValue: ""
+    };
+  },
+  methods: {
+    searchList() {
+      if (this.selectedCondition !== "선택 없음" && this.searchValue !== "") {
+        this.$emit("searchList", this.selectedCondition, this.searchValue);
+        this.clearValue();
+      }
+    },
+    clearValue() {
+      this.selectedCondition = "선택 없음";
+      this.searchValue = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
